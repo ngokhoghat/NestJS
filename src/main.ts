7f3e215f295f2d859f736fc5557d8eba34caf439
Your Name <you@example.com>
2021-01-27 07:12:10 +0700

    upadte

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestApplication, NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 
+import * as admin from 'firebase-admin';
+import * as serviceAccount  from './config/coffee-house-3eb7f-firebase-adminsdk-emt3h-677d69511f.json'
+
 import { AppModule } from './modules/app.module';
 import { join } from 'path';
 
 async function bootstrap() {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as any),
+    storageBucket: 'coffee-house-3eb7f.appspot.com'
+  });
+
   const app = await NestFactory.create<NestApplication>(AppModule);
 
   const logger = new Logger('Bootstrap');
--- src/modules/cats/cats.controller.ts
+++ src/modules/cats/cats.controller.ts
@@ -1,13 +1,33 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { CatsService } from './cats.service';
 
+import * as admin from 'firebase-admin';
+
 @Controller('cats')
 export class CatsController {
   constructor(private readonly appService: CatsService) { }
 
   @Get()
   getHello(): any {
-    return this.appService.findAll();
+    const bucket = admin.storage().bucket();
+    bucket.upload('./public/assets/images/coffee-house-bg.jpeg',
+    {
+      destination: `Image/coffee-house-bg.jpeg`,
+      gzip: true, 
+      contentType: "image/jpeg", 
+      metadata: {
+        metadata: {
+            firebaseStorageDownloadTokens: 'asdasd124124a2342sdasd34124asda'
+        }
+    },
+    })
+      .then((file: any) => {
+        console.log(file);
+
+      }).catch(err => {
+        console.log(err);
+      })
+    return ''
   }
 
   @Post()
